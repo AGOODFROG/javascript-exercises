50546629105c982efce297ebcaa173bcad5c8890
AGOODFROG <caden.j.quirion@gmail.com>
2024-01-07 16:44:11 -0500

    done all test passing
    this uses a nested for loop, I know but it works fine

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
+    const argumentsArray = Array.from(arguments)
 
+    let input = argumentsArray.shift()
+    const removeList = argumentsArray// at this point it the list of vaulus to be removed
+    let removeItem = undefined
+
+    for(let i of removeList){ 
+        removeItem = i
+        console.log(removeItem)
+        for(let j in input)if(input[j] === removeItem) input.splice(j,1)    
+        }
+        return input
 };
 
 // Do not edit below this line
