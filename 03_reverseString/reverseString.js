7ff44b2874654f9c5749895d12f1022d00d2799e
AGOODFROG <caden.j.quirion@gmail.com>
2024-01-07 15:03:55 -0500

    done all tests passing,
    I should work on cleaning code up, like on codewars, but still

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const textList = text.split("")// turns string in to arrray, 
+   let outList = []
+    for( let i of textList){
+        outList.unshift(i)
+    }
+    const output = outList.join("")
+    return output
 };
 
 // Do not edit below this line
