762d5b3d465d5822efb78602f969682f4627e77f
AGOODFROG <caden.j.quirion@gmail.com>
2024-01-07 13:14:54 -0500

    done task passing all tests, clean, crisp awsome

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str , numOfiterations) {
+    if(numOfiterations < 0) return "ERROR"
+    let out = ""
+    for(let i = 0; i < numOfiterations; i++)out += str
+    return out
+    
 };
 
 // Do not edit below this line
