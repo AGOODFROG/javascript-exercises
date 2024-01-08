918ae646993e8e56c9c80f50c2ffb1c3a87b0c11
AGOODFROG <caden.j.quirion@gmail.com>
2024-01-08 10:20:06 -0500

    removed pseudocode

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,15 +1,4 @@
 const leapYears = function(year) {
-    /*
-      if div by 4
-        if div by 100 and 400
-            true
-        else 
-            false 
-
-      if not div by 4
-        retuern flase 
-    
-    */
    if( year % 4 === 0){
         if( year % 100 === 0 && year % 400 !== 0 ) return false
         return true    
