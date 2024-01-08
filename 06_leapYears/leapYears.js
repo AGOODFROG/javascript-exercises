68145d9aa7de6708633d2fe66c9b56377a7b9bff
AGOODFROG <caden.j.quirion@gmail.com>
2024-01-08 10:18:23 -0500

    done all tests passing

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    /*
+      if div by 4
+        if div by 100 and 400
+            true
+        else 
+            false 
 
+      if not div by 4
+        retuern flase 
+    
+    */
+   if( year % 4 === 0){
+        if( year % 100 === 0 && year % 400 !== 0 ) return false
+        return true    
+   }else return false
 };
 
 // Do not edit below this line
