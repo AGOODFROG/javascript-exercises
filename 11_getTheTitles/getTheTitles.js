b2aca0362181db67d37a7bdfbd68692afa69b713
AGOODFROG <caden.j.quirion@gmail.com>
2024-01-10 13:20:48 -0500

    too easy, i must have done something bad

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookList) {
+    let output = []
+    for(let i of bookList) output.push(i.title)
+    return output
 };
 
 // Do not edit below this line
