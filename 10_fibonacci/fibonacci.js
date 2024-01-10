a399e2fff1a8f63d0c7c47dfe6f287dbfc931c14
AGOODFROG <caden.j.quirion@gmail.com>
2024-01-10 13:11:13 -0500

    done all test passing
    works by flitering out edge case then iterating the math with the
    first two already done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(maxNumber) {
+    maxNumber = Number(maxNumber)
+    if(maxNumber === 0) return 0 // IDK some proof most be some where
+    if(maxNumber < 0) return "OOPS"
+    const fibonacciNumbers = [1, 1]// the base to avoid indexing errors
+    const oneBefore = 1
+    const twoBefor = 2
+    for(let i = 2; i < maxNumber; i++){
+        fibonacciNumbers.push(
+            fibonacciNumbers[i-oneBefore] + 
+            fibonacciNumbers[i-twoBefor] 
+            )    
+    }
+    return fibonacciNumbers[fibonacciNumbers.length - 1]
 };
 
 // Do not edit below this line
