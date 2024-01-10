b5d7d44d73c931fd5f1db621d416ab0015af2d0b
AGOODFROG <caden.j.quirion@gmail.com>
2024-01-10 15:42:52 -0500

    done all test passing
    yes I know there is an easyer way to do this. I wanted to pay
    forword for when I am making each-a-skech and stuff

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(input) {
+    let prevAge = 0
+    let cerentAge = 0// IN THE FOR LOOP
+    let prevObj = input[0]// get it ready to be looped over
+
+    for(let i of input){
+        if( i.yearOfDeath === undefined){
+            const d = new Date()
+            let year = d.getFullYear()
+            cerentAge = year - i.yearOfBirth
+        } else cerentAge = i.yearOfDeath - i.yearOfBirth
+        
+        if (cerentAge > prevAge){
+            prevAge = cerentAge
+            prevObj = i
+        } 
+    }
+    return prevObj
+    
 
 };
 
