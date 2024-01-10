f0b7188ed852195febace6b99c9b126666c58238
AGOODFROG <caden.j.quirion@gmail.com>
2024-01-09 21:38:43 -0500

    passing all test, code tell its self

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	 return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let out = 0
+	for(let i of numbers){
+    out += i
+  }
+  return out
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let out = 1
+  for(let i of numbers) out *= i
+  return out
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	 return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+  let out = 1
+	for(let i = input; i > 0; i--)out *= i
+  return out
 };
 
 // Do not edit below this line
